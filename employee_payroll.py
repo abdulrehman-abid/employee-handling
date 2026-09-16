3f6eca7495e15c4943217d51b2092454c6daeb10
Abdulrehaman <aispecialist3333>
2026-09-16 11:27:56 +0500

    used OOps correctly and enhanced it added privacy

--- employee_payroll.py
+++ employee_payroll.py
@@ -1,10 +1,12 @@
 """======================================================= employee payroll============================================================"""
-"""============================ Imports ============================"""
+"""============================ Collecting Raw Data ============================"""
 from datetime import datetime
 now = datetime.now().strftime("%Y-%m-%d")
+
 from getpass import getpass
-"""===================== Database ======================="""
 import mysql.connector
+
+"""===================== Database ======================="""
 password = getpass("Enter your password of database: ")
 connection = mysql.connector.connect(
     host = "localhost",
@@ -15,31 +17,33 @@ connection = mysql.connector.connect(
 
 cursor = connection.cursor()
 """======================= Engine ======================="""
+
 def find_employee_CNIC():
     while True:
-                try:
-                    employee_CNIC = (input("Enter your CNIC: ")).replace("-", "").strip()
-               
-                    if len(employee_CNIC) != 13 or not employee_CNIC.isdigit():
-                        print( "Invalid CNIC OR It should be 13 digits long.")
-                        continue
-                    else:
-                        cursor.execute("SELECT * FROM emp_info where CNIC = %s",(employee_CNIC, ))
-                        data = cursor.fetchone()
-    
-                        if not data:
-                            print("This CNIC is not registered.")
-                            continue
-    
-                        return employee_CNIC
-    
-                except ValueError:
-                    print("Invalid input. Please enter a valid CNIC number.")
+        try:
+            employee_CNIC = input("Enter your CNIC: ").replace("-", "").strip()
+        
+            if len(employee_CNIC) != 13 or not employee_CNIC.isdigit():
+                print("Invalid CNIC OR It should be 13 digits long.")
+                continue
+            else:
+                cursor.execute("SELECT * FROM emp_info where CNIC = %s",(employee_CNIC, ))
+                data = cursor.fetchone()
+
+                if not data:
+                    print("This CNIC is not registered.")
+                    continue
+
+                return employee_CNIC
+
+        except ValueError:
+            print("Invalid input. Please enter a valid CNIC number.")
+
 def get_new_CNIC():
     while True:
             try:
-                employee_CNIC = (input("Enter your CNIC: ")).replace("-", "").strip()
-           
+                employee_CNIC = input("Enter your CNIC: ").replace("-", "").strip()
+        
                 if len(employee_CNIC) != 13 or not employee_CNIC.isdigit():
                     print( "Invalid CNIC. It should be 13 digits long.")
                     continue
@@ -56,133 +60,141 @@ def get_new_CNIC():
             except ValueError:
                 print("Invalid input. Please enter a valid CNIC number.")
 def name():
-    return input("Enter your name: ").strip() 
+    return input("Enter your name: ").strip()
 
-def register_employee():
-    employee_name = name()
-    employee_CNIC = get_new_CNIC()
-    sql = """
-    INSERT INTO emp_info(name, CNIC)
-    VALUES(%s, %s)
-    """
-    values = (employee_name, employee_CNIC)
+class Employee:
+    def __init__(self, name, CNIC):
+        self.name = name
+        self.CNIC = CNIC
 
-    cursor.execute(sql, values)
-    connection.commit()
-    return "employee registered successfully"
+    def employee_id(self):
+        sql = """SELECT id FROM emp_info WHERE CNIC = %s"""
+        values = (self.CNIC, )
+        cursor.execute(sql, values)
+        data = cursor.fetchone()[0]
+        return data
+ 
+    def register_employee(self):
+        sql = """
+        INSERT INTO emp_info(name, CNIC)
+        VALUES(%s, %s)
+        """
+        values = (self.name, self.CNIC)
 
-class salary_calculator():
-    def __init__(self):
-        self.salary = self.calculate_salary()
+        cursor.execute(sql, values)
+        connection.commit()
+        return f"{self.name} is registered successfully"
 
+    
     def calculate_salary(self):
-        search_CNIC = find_employee_CNIC()
+        search_CNIC = self.CNIC
         total_hours = 0
         
 
-        sql = """SELECT id FROM emp_info where CNIC = %s"""
-        Values = (search_CNIC, )
-        cursor.execute(sql, Values)
-
-        employee_id = cursor.fetchone()[0]
              
-        cursor.execute("SELECT sum(hours_worked) FROM work_hours where employee_id = %s",(employee_id, ))
+        cursor.execute("SELECT sum(hours_worked) FROM work_hours where employee_id = %s",(self.employee_id(), ))
         total_hours = cursor.fetchone()[0]
         if not total_hours :
-            print(f"No hours found for this CNIC: {search_CNIC}")
+            return f"No hours found for this CNIC: {search_CNIC}"
 
         else:
-            rate = float(input("Enter the hourly rate: "))
+            rate = int(input("Enter the hourly rate: "))
             salary = total_hours * rate
             return f"Your salary is: {salary} Rupees for {total_hours} hours"
-        
-
-
-        
-        
+    
     def __repr__(self):
-        return (f"Salary Calculator: {self.salary}")
+        return (f"Salary Calculator: {self.calculate_salary()}")
 
-class log_daily_hours():
-    def __init__ (self):
-        self.is_registered = self.is_registered()
-        self.log_hours = self.log_hours()
 
-    def is_registered(self):
-        return find_employee_CNIC()
 
     def log_hours(self):
-        if self.is_registered != None:
-            while True:
-                try:
-                    hours = int (input("Enter the hours worked today:"))
-                except ValueError:
-                    print("Invalid input. Please enter a valid number.")
-                    return
-                if hours < 0 or hours > 24:
-                    print("Invalid input. Please enter a number between 0 and 24.")
-                else:
-                    sql = """SELECT id FROM emp_info WHERE CNIC = %s"""
-                    values = (self.is_registered, )
-                    cursor.execute(sql, values)
-                    data = cursor.fetchone()[0]
+        
+        while True:
+            try:
+                hours = int (input("Enter the hours worked today:"))
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+                continue
+            if hours < 0 or hours > 24:
+                print("Invalid input. Please enter a number between 0 and 24.")
+                continue
+            else:
+                sql = """SELECT id FROM emp_info WHERE CNIC = %s"""
+                values = (self.CNIC, )
+                cursor.execute(sql, values)
+                data = cursor.fetchone()[0]
 
 
-                    sql = """INSERT INTO work_hours(employee_id, work_date, hours_worked)
-                    VALUES(%s, %s, %s)"""
-                    values = (data, now, hours)
-                    cursor.execute(sql, values)
-                    connection.commit()
-                    return f"You have logged {hours} hours"
-                break
+                sql = """INSERT INTO work_hours(employee_id, work_date, hours_worked)
+                VALUES(%s, %s, %s)"""
+                values = (data, now, hours)
+                cursor.execute(sql, values)
+                connection.commit()
+                return f"You have logged {hours} hours"
 
-class get_emp_info():
-    def __init__(self):
-        self.emp_info = self.get_emp_info()
 
-    def get_emp_info(self):
-        sql = """SELECT * FROM emp_info"""
-        cursor.execute(sql)
+    def employee_fetchall(self):
+
+        sql = """SELECT * FROM emp_info where CNIC = %s"""
+        values = (self.CNIC, )
+        cursor.execute(sql, values)
         data = cursor.fetchall()
         return data
-class get_hours_worked():
-    def __init__(self):
-        self.hours_worked = self.get_hours_worked()
-
+    
     def get_hours_worked(self):
-        sql = """SELECT * FROM work_hours"""
-        cursor.execute(sql)
+        sql = """SELECT * FROM work_hours where employee_id = %s"""
+        values = (self.employee_id(), )
+        cursor.execute(sql, values, )
         data = cursor.fetchall()
         return data
 
 """======================= User Interface ======================="""
 
+menu_options = [
+
+    "Add a new employee",
+    "Log hours",
+    "Calculate salary",
+    "Get employee info",
+    "Get hours worked",
+    "Exit"
+]
+
+print("\n================ Welcome to Salary Calculator ================")
+print("Welcome to Salary Calculator")
+menu_string = ""
+for index, options in enumerate(menu_options, start=1):
+    menu_string += (f"{index}. {options}\n")
+n = len(menu_options)
 
 while True:
-    print("\n================ Welcome to Salary Calculator ================")
-    print("Welcome to Salary Calculator")
-    print("1.Add a new employee")
-    print("2.Log hours")
-    print("3.calculate salary")
-    print("4.Get employee info")
-    print("5.Get hours worked")
-    print("6.Exit")
+    print(menu_string)
     try:
-        user_input = int(input("Enter your choice(1-6): "))
+        user_input = int(input(f"Enter your choice(1-{n}): "))
     except ValueError:
-        print("Invalid input. Please enter a number between 1 and 6.")
+        print(f"Invalid input. Please enter a number between 1 and {n}.")
         continue
     if user_input == 1:
-        register_employee()
+        employee_name = name()
+        employee_CNIC = get_new_CNIC()
+        employee = Employee(employee_name, employee_CNIC)
+        print(employee.register_employee())
     elif user_input == 2:
-        log_daily_hours()
+        employee_CNIC = find_employee_CNIC()
+        employee = Employee("", employee_CNIC)
+        print(employee.log_hours())
     elif user_input == 3:
-        print(salary_calculator())
-    
+        employee_CNIC = find_employee_CNIC()
+        employee = Employee("", employee_CNIC)
+        print(employee.calculate_salary())
     elif user_input == 4:
-        print(get_emp_info())
+        employee_CNIC = find_employee_CNIC()
+        employee = Employee("", employee_CNIC)
+        print(employee.employee_fetchall())
     elif user_input == 5:
-        print(get_hours_worked())
+        employee_CNIC = find_employee_CNIC()
+        employee = Employee("", employee_CNIC)
+        print(employee.get_hours_worked())
     elif user_input == 6:
         print("Thank you for using Salary Calculator")
 
@@ -194,4 +206,4 @@ while True:
 
         break
     else:
-        print("Invalid input. Please enter a number between 1 and 6.")  
+        print(f"Invalid input. Please enter a number between 1 and {n}.")  
